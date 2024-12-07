58017f805ed506e8e42468d8041466944dec6faf
mikastiv <mikastiv@outlook.com>
2024-12-06 22:49:15 -0500

    refactor day6

--- src/day6.zig
+++ src/day6.zig
@@ -12,19 +12,16 @@ const Dir = packed struct {
     y: isize,
 
     fn turn(self: Dir) Dir {
-        return if (self.x == 1 or self.x == -1) .{
-            .y = self.x,
-            .x = 0,
-        } else .{
+        return .{
             .x = -self.y,
-            .y = 0,
+            .y = self.x,
         };
     }
 };
 
 fn startPos(grid: []const []const u8) ?Pos {
     for (grid, 0..) |line, y| {
-        if (std.mem.indexOfAny(u8, line, "^v<>")) |x| {
+        if (std.mem.indexOfScalar(u8, line, '^')) |x| {
             return .{ .x = @intCast(x), .y = @intCast(y) };
         }
     }
@@ -32,61 +29,36 @@ fn startPos(grid: []const []const u8) ?Pos {
     return null;
 }
 
-fn startDir(grid: []const []const u8, pos: Pos) Dir {
-    return switch (grid[@intCast(pos.y)][@intCast(pos.x)]) {
-        '^' => .{ .x = 0, .y = -1 },
-        'v' => .{ .x = 0, .y = 1 },
-        '<' => .{ .x = -1, .y = 0 },
-        '>' => .{ .x = 1, .y = 0 },
-        else => unreachable,
-    };
-}
-
-pub fn main() !void {
-    const row_count = std.mem.count(u8, input, "\n");
-
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const alloc = arena.allocator();
-
-    var grid = try alloc.alloc([]u8, row_count);
-
-    var lines = std.mem.tokenizeAny(u8, input, &std.ascii.whitespace);
-    var index: usize = 0;
-    while (lines.next()) |line| : (index += 1) {
-        grid[index] = try alloc.dupe(u8, line);
-    }
-
-    const start_pos = startPos(grid) orelse return error.NoStartingPosition;
-    const start_dir = startDir(grid, start_pos);
-
+fn countVisited(alloc: std.mem.Allocator, grid: []const []const u8, start_pos: Pos, start_dir: Dir) !usize {
     var visited = std.AutoHashMap(Pos, void).init(alloc);
-    {
-        var dir = start_dir;
-        var pos = start_pos;
-        while (true) {
-            try visited.put(pos, {});
 
-            const next_pos: Pos = .{
-                .x = pos.x + dir.x,
-                .y = pos.y + dir.y,
-            };
+    var dir = start_dir;
+    var pos = start_pos;
+    while (true) {
+        try visited.put(pos, {});
 
-            if (next_pos.x < 0 or next_pos.x >= grid[0].len) break;
-            if (next_pos.y < 0 or next_pos.y >= grid.len) break;
+        const next_pos: Pos = .{
+            .x = pos.x + dir.x,
+            .y = pos.y + dir.y,
+        };
 
-            if (grid[@intCast(next_pos.y)][@intCast(next_pos.x)] == '#') {
-                dir = dir.turn();
-            } else {
-                pos = next_pos;
-            }
+        if (next_pos.x < 0 or next_pos.x >= grid[0].len) break;
+        if (next_pos.y < 0 or next_pos.y >= grid.len) break;
+
+        if (grid[@intCast(next_pos.y)][@intCast(next_pos.x)] == '#') {
+            dir = dir.turn();
+        } else {
+            pos = next_pos;
         }
     }
 
-    const part1 = visited.count();
+    return visited.count();
+}
 
+fn countLoops(alloc: std.mem.Allocator, grid: []const []u8, start_pos: Pos, start_dir: Dir) !usize {
     var turn_history = std.AutoHashMap(Pos, void).init(alloc);
 
-    var part2: usize = 0;
+    var loops: usize = 0;
     for (grid) |line| {
         for (line) |*char| {
             if (char.* != '.') continue;
@@ -108,10 +80,12 @@ pub fn main() !void {
                 if (grid[@intCast(next_pos.y)][@intCast(next_pos.x)] == '#') {
                     dir = dir.turn();
 
-                    const entry = try turn_history.getOrPut(pos);
-                    if (entry.found_existing and moved) {
-                        part2 += 1;
-                        break;
+                    if (moved) {
+                        const entry = try turn_history.getOrPut(pos);
+                        if (entry.found_existing) {
+                            loops += 1;
+                            break;
+                        }
                     }
 
                     moved = false;
@@ -125,6 +99,29 @@ pub fn main() !void {
             char.* = '.';
         }
     }
+
+    return loops;
+}
+
+pub fn main() !void {
+    const row_count = std.mem.count(u8, input, "\n");
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const alloc = arena.allocator();
+
+    var grid = try alloc.alloc([]u8, row_count);
+
+    var lines = std.mem.tokenizeAny(u8, input, &std.ascii.whitespace);
+    var index: usize = 0;
+    while (lines.next()) |line| : (index += 1) {
+        grid[index] = try alloc.dupe(u8, line);
+    }
+
+    const start_pos = startPos(grid) orelse return error.NoStartingPosition;
+    const start_dir: Dir = .{ .x = 0, .y = -1 };
+
+    const part1 = try countVisited(alloc, grid, start_pos, start_dir);
+    const part2 = try countLoops(alloc, grid, start_pos, start_dir);
 
     std.debug.print("part1: {d}\n", .{part1});
     std.debug.print("part2: {d}\n", .{part2});
